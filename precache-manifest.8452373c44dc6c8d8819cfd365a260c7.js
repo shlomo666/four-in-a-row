96041ce3ab815088bc45d867339b4d6ffe639033
shlomo666 <shlomoh@homeis.com>
2020-01-22 10:08:38 +0200

    Updates

---
+++ precache-manifest.8452373c44dc6c8d8819cfd365a260c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a17f3ba8a48d9686014e5a0763556a",
+    "url": "/four-in-a-row/index.html"
+  },
+  {
+    "revision": "faf02a530b74e1d197f8",
+    "url": "/four-in-a-row/static/css/2.96479aa2.chunk.css"
+  },
+  {
+    "revision": "cf01778e1ed7a7d14962",
+    "url": "/four-in-a-row/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "faf02a530b74e1d197f8",
+    "url": "/four-in-a-row/static/js/2.2029c574.chunk.js"
+  },
+  {
+    "revision": "a43ccc6eaceb95634adbc1f97bd71b22",
+    "url": "/four-in-a-row/static/js/2.2029c574.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf01778e1ed7a7d14962",
+    "url": "/four-in-a-row/static/js/main.7b0ffcf0.chunk.js"
+  },
+  {
+    "revision": "8dd0b3a784579d0c70be",
+    "url": "/four-in-a-row/static/js/runtime-main.b48e5d64.js"
+  }
+]);
